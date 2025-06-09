a515cb7bf89a8dba8fb7ca2b06d87ed19c0911db
Thales <jiangtai20@mails.ucas.ac.cn>
2025-06-09 22:39:18 +0800

    style change

--- src/views/chat/plan-map.vue
+++ src/views/chat/plan-map.vue
@@ -4,8 +4,15 @@ import { GoogleMap, Marker } from 'vue3-google-map';
 const apiKey = import.meta.env.IPG_GOOGLE_KEY;
 const center = { lat: 39.980021, lng: 116.332241 };
 const mapRef = ref<typeof GoogleMap | null>(null);
+
 function addMarker() {
   mapRef.value?.map.panTo({ lat: 39.981171, lng: 116.247504 });
+}
+
+function onSearch() {
+  fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=%E8%87%AA%E5%8A%A8%E5%8C%96%E7%A0%94%E7%A9%B6%E6%89%80&key=AIzaSyDG1OwabKoD6wmMgWp_HxoNY_J7GgxvAO8').then((res) =>
+    console.log(res),
+  );
 }
 
 onMounted(() => {
@@ -18,4 +25,5 @@ onMounted(() => {
     <Marker :options="{ position: center }" />
   </google-map>
   <button @click="addMarker">change</button>
+  <button @click="onSearch">search</button>
 </template>
