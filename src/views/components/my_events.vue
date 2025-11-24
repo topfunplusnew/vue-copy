fadc88fa0449d90ff2b133930bb7668c303398e9
wy <2589076661@qq.com>
2025-11-24 22:55:17 +0800

    paper页面的conference增加router

--- src/views/components/my_events.vue
+++ src/views/components/my_events.vue
@@ -448,7 +448,7 @@ function getAffiliationNumber(originalId: number): number {
               <img :src="getImageUrl(myPaperDetailInfo.conference.logo)" alt="Conference Logo" />
             </div>
             <div class="conference-info">
-              <div class="conference-name">{{ myPaperDetailInfo?.conference.abbreviation }}</div>
+              <router-link :to="{name:'FeaturedEvents',params:{conferenceId:myPaperDetailInfo?.conference.id}}" class="conference-name">{{ myPaperDetailInfo?.conference.abbreviation }}</router-link>
               <div class="conference-full-name">{{ myPaperDetailInfo?.conference.name }}</div>
             </div>
           </div>
--- src/views/components/paper-details.vue
+++ src/views/components/paper-details.vue
@@ -378,7 +378,7 @@ const getFlattenedReplies = (replies: Comment[] | undefined): Array<{ reply: Com
                 <img :src="getImageUrl(paperDetail.conference.logo)" :alt="paperDetail.conference.abbreviation" />
               </div>
               <div class="conference-info">
-                <div class="conference-name">{{ paperDetail?.conference?.abbreviation }}</div>
+                <router-link :to="{name:'FeaturedEvents',params:{conferenceId:paperDetail?.conference?.id}}" class="conference-name">{{ paperDetail?.conference?.abbreviation }}</router-link>
                 <div class="conference-full-name">{{ paperDetail?.conference?.name }}</div>
               </div>
             </div>
