0b12ce861e1da4f180cadbe5fcf6109fdde625b5
topfun <ming10655@gmail.com>
2025-11-25 22:34:43 +0800

    添加notes

--- src/services/paper/index.ts
+++ src/services/paper/index.ts
@@ -4,6 +4,10 @@ import type {
   UpdatePaperCommentData,
   GetPaperCommentsResponse,
   PaperCommentResponse,
+  PaperNoteData,
+  GetPaperNotesResponse,
+  GetPaperNoteResponse,
+  PaperNoteResponse,
 } from './type';
 
 /**
@@ -42,3 +46,45 @@ export const updatePaperComment = (paperId: number | string, data: UpdatePaperCo
       'Content-Type': 'application/json',
     },
   });
+
+/**
+ * 为论文添加笔记
+ * @param paperId 论文ID
+ * @param data 笔记数据
+ * @param data.content 笔记内容
+ * @returns Promise 返回创建的笔记
+ */
+export const createPaperNote = (paperId: number | string, data: PaperNoteData) =>
+  http.post<PaperNoteResponse>(`/paper/${paperId}/notes`, data, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+/**
+ * 修改论文笔记
+ * @param paperId 论文ID
+ * @param data 笔记数据
+ * @param data.content 笔记内容
+ * @returns Promise 返回更新后的笔记
+ */
+export const updatePaperNote = (paperId: number | string, data: PaperNoteData) =>
+  http.put<PaperNoteResponse>(`/paper/${paperId}/notes`, data, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+/**
+ * 查看用户所有的笔记列表
+ * @returns Promise 返回笔记列表
+ */
+export const getPaperNotes = () => http.get<GetPaperNotesResponse>('/paper/notes');
+
+/**
+ * 查询某篇论文的笔记
+ * @param paperId 论文ID
+ * @returns Promise 返回笔记信息
+ */
+export const getPaperNote = (paperId: number | string) =>
+  http.get<GetPaperNoteResponse>(`/paper/${paperId}/notes`);
